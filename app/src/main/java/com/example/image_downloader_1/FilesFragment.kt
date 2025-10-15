73c155517b6d945d999d2f77b508814f55515ba2
rahulrajesh21 <rahulrajesh21@gmail.com>
2025-10-15 12:28:25 +0530

    Add download dialog UI with custom styling and radio selectors

--- app/src/main/java/com/example/image_downloader_1/FilesFragment.kt
+++ app/src/main/java/com/example/image_downloader_1/FilesFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
 class FilesFragment: Fragment() {
 
     override fun onCreateView(
@@ -12,7 +16,23 @@ class FilesFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ):View?{
-        return inflater.inflate(R.layout.fragment_download, container, false)
+        val view = inflater.inflate(R.layout.fragment_download,container,false)
+        val add = view.findViewById<FloatingActionButton>(R.id.floatingActionButton2)
+
+        add.setOnClickListener {showDialogBox()}
+        return view
+    }
+
+    private fun showDialogBox(){
+        val editText = EditText(requireContext())
+        val dialogbox = layoutInflater.inflate(R.layout.dialog_download,null)
+
+        AlertDialog.Builder(requireContext())
+            .setView(dialogbox)
+            .create()
+            .show()
+
+
     }
 
 }
